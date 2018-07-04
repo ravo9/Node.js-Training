a44041b70eeb4394a457032028a5b6a2e4f1d18a
Rafal Ozog <rafalozog@gmail.com>
2018-07-04 20:50:02 +0100

    Repo update

--- project01/app.js
+++ project01/app.js
@@ -44,6 +44,43 @@ const files = fs.readdir('./', function(err, files){
 
 // ----------
 
-const EventEmitter = require('events');
+/*const EventEmitter = require('events');
 
-const emitter = new EventEmitter();
+
+const Logger = require('./logger');
+const logger = new Logger();
+
+
+// Register a listener
+logger.on('messageLogged', function(arg) {
+    console.log('Listener called', arg);
+});
+
+
+logger.log('My message 01');*/
+
+// ----------
+
+const http = require('http');
+
+const server = http.createServer( (req, res) => {
+
+    if (req.url === '/') {
+        res.write("Hello world");
+        res.end();
+    }
+
+    if (req.url === '/api/courses') {
+        res.write(JSON.stringify([1, 2, 3, 4]));
+        res.end();
+    }
+});
+
+/*// Setting listener
+server.on('connection', (socket) => {
+    console.log('New connection...');
+})*/
+
+// Listen on port 3000
+server.listen(3000);
+console.log('Listening on port 3000');
